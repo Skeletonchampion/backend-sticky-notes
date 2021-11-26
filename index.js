1003e124f94bc0ad32872e9c4c9c2adfd73234ec
Skeletonchampion <greensproblem00@gmail.com>
2021-11-26 15:20:45 +0700

    fix

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ app.use(cors({
     })
 );
 
-mongoose.connect(process.env.DB_URL, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect("mongodb+srv://givememeso2:1@cluster0.yfmos.mongodb.net/stickynotes-users", { useNewUrlParser: true, useUnifiedTopology: true });
 
 app.get("/user", (req, res) => {
     res.json({userId: req.session.userId, username: req.session.username});
